2d2de1b0d25480e89e94985d6be169df91e9c110
Vansh Kharbanda <vansh.kharbanda@student.manchester.ac.uk>
2023-11-30 16:18:21 +0000

    error.logs storing data where no data could be added to the vocabulary file

--- vocabulary_script.py
+++ vocabulary_script.py
@@ -1,8 +1,9 @@
 import os
 import json
+import traceback
 
 
-def process_folder(folder_path):
+def process_folder(folder_path, error_log):
     """
     Input: path of the folder with json files present
 
@@ -10,7 +11,7 @@ def process_folder(folder_path):
     """
     vocabulary_data = {}
 
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(folder_path,):
             # print(filename)
             if filename.endswith('biotools.json'):
                 file_path = os.path.join(folder_path, filename)
@@ -43,7 +44,9 @@ def process_folder(folder_path):
                         if (biotools_id is not None) and (collection_id is not None):
                             vocabulary_data[biotools_id] = collection_id
                     except Exception as e:
-                        print(f'Error: {e} in file: {file_path}')
+                        # print(f'Error: {e} in file: {file_path}')
+                        error_log.write(f'Error: {e} in file: {file_path}\n')
+                        traceback.print_exc(file=error_log)
                          
     return vocabulary_data
 
@@ -51,21 +54,22 @@ def iterate_folder():
     root_folder = f'../research-software-ecosystem-content/data'
     final_vocabulary = {}
 
-    count = 0
-    # Iterate over subdirectories in the root folder
-    for subdir in os.listdir(root_folder):
-        subdir_path = os.path.join(root_folder, subdir)
-        # print(subdir_path)
-        count += 1
-        # if count > 100:
-        #      break
+    with open('error.log', 'w') as error_log:
+        count = 0
+        # Iterate over subdirectories in the root folder
+        for subdir in os.listdir(root_folder):
+            subdir_path = os.path.join(root_folder, subdir)
+            # print(subdir_path)
+            count += 1
+            # if count > 100:
+            #      break
 
-        # Check if it's a directory
-        if os.path.isdir(subdir_path):
-            # Process the folder and update the final vocabulary data
-            folder_data = process_folder(subdir_path)
-            final_vocabulary.update(folder_data)
-            # print(final_vocabulary)
+            # Check if it's a directory
+            if os.path.isdir(subdir_path):
+                # Process the folder and update the final vocabulary data
+                folder_data = process_folder(subdir_path, error_log)
+                final_vocabulary.update(folder_data)
+                # print(final_vocabulary)
 
     # # Write the final vocabulary data to 'vocabulary.json'
     with open('vocabulary.json', 'w') as vocab_file:
