ff7d05b1063406c8f8569782087a1f5461fae97c
Vansh Kharbanda <vansh.kharbanda@student.manchester.ac.uk>
2023-11-30 15:23:29 +0000

    Initial Vocabulary created - different refrences in data- fix it

---
+++ vocabulary_script.py
@@ -0,0 +1,74 @@
+import os
+import json
+
+
+def process_folder(folder_path):
+    """
+    Input: path of the folder with json files present
+
+    Output: dictionary with "biotoolsID" as key and "collectionID" as value
+    """
+    vocabulary_data = {}
+
+    for filename in os.listdir(folder_path):
+            # print(filename)
+            if filename.endswith('biotools.json'):
+                file_path = os.path.join(folder_path, filename)
+                # print(file_path)
+
+                # Read the contents of the biotools.json file
+                with open(file_path, 'r') as file:
+                    biotools_data = json.load(file)
+
+                    # Extract biotoolsID and CollectionID
+                    try:
+                        biotools_id = biotools_data.get('biotoolsID')
+                        collection_id = biotools_data.get('CollectionID')
+                        if collection_id is None:
+                            collection_id = biotools_data.get('topic')
+                            if collection_id:
+                                terms = []
+                                for i in range (len(collection_id)):
+                                    terms.append(collection_id[i]['term'])
+                            collection_id = terms                   
+                        # biotools_id = biotools_data['biotoolsID']
+                        # collection_id = biotools_data['CollectionID']
+                        # print(biotools_id, collection_id)
+
+                        # # Add to vocabulary_data dictionary
+                        if (biotools_id is not None) and (collection_id is not None):
+                            vocabulary_data[biotools_id] = collection_id
+                    except Exception as e:
+                         print(f'Error: {e}')
+                         
+    return vocabulary_data
+
+def iterate_folder(folder_name = 'uniprotkb'):
+    root_folder = f'../research-software-ecosystem-content/data'
+    # root_folder = f'../research-software-ecosystem-content/data/{folder_name}'
+    # root_folder = '../research-software-ecosystem-content/data/uniprotkb/uniprotkb.biotools.json'
+    final_vocabulary = {}
+
+    count = 0
+    # Iterate over subdirectories in the root folder
+    for subdir in os.listdir(root_folder):
+        subdir_path = os.path.join(root_folder, subdir)
+        # print(subdir_path)
+        count += 1
+        # if count > 100:
+        #      break
+
+        # Check if it's a directory
+        if os.path.isdir(subdir_path):
+            # Process the folder and update the final vocabulary data
+            folder_data = process_folder(subdir_path)
+            final_vocabulary.update(folder_data)
+            # print(final_vocabulary)
+
+    # # Write the final vocabulary data to 'vocabulary.json'
+    with open('vocabulary.json', 'w') as vocab_file:
+        json.dump(final_vocabulary, vocab_file, indent=4)
+        
+    print(f'Total number of folders parsed: {count}')
+
+iterate_folder()
